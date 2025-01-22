11f30f3aab85fbbe5047f810a42defa2755d9207
Mark-Tevin <tevinsmark@gmail.com>
2025-01-22 12:35:48 +0300

    Accounts backend-logic

--- app/layout.js
+++ app/layout.js
@@ -2,8 +2,10 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "sonner";
 
-const inter = Inter({subsets: ["latin"] });
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "PesaMind",
@@ -14,19 +16,20 @@ export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
       <html lang="en">
-      <body suppressHydrationWarning className={`${inter.className}`}>
-        {/* Header */}
-        <Header/>
-
-        <main className="min-h-screen">{children}</main>
-        {/* Footer */}
-        <footer className="bg-blue-50 py-12">
-          <div className="container mx-auto px-4 text-center text-gray-600">
-            <p>PesaMind: Smarter Choices, Better Finances</p>
-          </div>
-        </footer>
+        <body suppressHydrationWarning className={`${inter.className}`}>
+          {/* Header */}
+          <Header/>
+          <main className="min-h-screen">{children}</main>
+          <Toaster richColors />
+          
+          {/* Footer */}
+          <footer className="bg-blue-50 py-12">
+            <div className="container mx-auto px-4 text-center text-gray-600">
+              <p>PesaMind: Smarter Choices, Better Finances</p>
+            </div>
+          </footer>
         </body>
-    </html>
+      </html>
     </ClerkProvider>
   );
 }
