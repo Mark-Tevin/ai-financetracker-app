34ac3703f6b4365b681ddc39132854bc6d7354bf
Mark-Tevin <tevinsmark@gmail.com>
2025-01-24 02:47:59 +0300

    arcjet shield and bot protection

--- app/(main)/dashboard/_components/transaction-overview.jsx
+++ app/(main)/dashboard/_components/transaction-overview.jsx
@@ -165,7 +165,7 @@ export function DashboardOverview({ accounts, transactions }) {
                     cx="50%"
                     cy="50%"
                     outerRadius={80}
-                    fill="#8884d8"
+                    fill="#25e138"
                     dataKey="value"
                     label={({ name, value }) => `${name}: $${value.toFixed(2)}`}
                   >
