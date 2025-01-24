5183185cfda16607fa4cb00e52b4658d61f7a08c
Mark-Tevin <tevinsmark@gmail.com>
2025-01-24 04:59:36 +0300

    application debug

--- app/(main)/account/[id]/page.jsx
+++ app/(main)/account/[id]/page.jsx
@@ -1,29 +1,29 @@
+import { Suspense } from "react";
 import { getAccountWithTransactions } from "@/actions/account";
-import { notFound } from "next/navigation";
-import React, { Suspense } from "react";
-import TransactionTable from "../_components/transaction-table";
 import { BarLoader } from "react-spinners";
+import { TransactionTable } from "../_components/transaction-table";
+import { notFound } from "next/navigation";
 import { AccountChart } from "../_components/account-chart";
 
-const AccountsPage = async ({ params }) => {
-  const acccountData = await getAccountWithTransactions(params.id);
+export default async function AccountPage({ params }) {
+  const accountData = await getAccountWithTransactions(params.id);
 
-  if (!acccountData) {
+  if (!accountData) {
     notFound();
   }
-
 
   const { transactions, ...account } = accountData;
 
   return (
-    <div className="space-y-8 px-4 ">
-      <div className="flex gap-4 items-end j">
+    <div className="space-y-8 px-5">
+      <div className="flex gap-4 items-end justify-between">
         <div>
-          <h1 className="text-5xl sm:text-6xl font-bold gradient-title capitalise">
-          {account.name}
+          <h1 className="text-5xl sm:text-6xl font-bold tracking-tight gradient-title capitalize">
+            {account.name}
           </h1>
           <p className="text-muted-foreground">
-          {account.type.charAt(0) + account.type.slice(1).toLowerCase()} Account
+            {account.type.charAt(0) + account.type.slice(1).toLowerCase()}{" "}
+            Account
           </p>
         </div>
 
@@ -32,25 +32,24 @@ const AccountsPage = async ({ params }) => {
             ${parseFloat(account.balance).toFixed(2)}
           </div>
           <p className="text-sm text-muted-foreground">
-            {account._count.transactions} Transactions 
+            {account._count.transactions} Transactions
           </p>
         </div>
       </div>
 
       {/* Chart Section */}
-      <Suspense 
-        fallback={<BarLoader className="mt-4" width={"100%"} color="#9333ea"/>}
+      <Suspense
+        fallback={<BarLoader className="mt-4" width={"100%"} color="#9333ea" />}
       >
-        <AccountChart transactions={transactions} /> 
+        <AccountChart transactions={transactions} />
       </Suspense>
-      {/* Transaction Table */}
-      <Suspense 
-        fallback={<BarLoader className="mt-4" width={"100%"} color="#9333ea"/>}
+
+      {/* Transactions Table */}
+      <Suspense
+        fallback={<BarLoader className="mt-4" width={"100%"} color="#9333ea" />}
       >
-        <TransactionTable transactions={transactions}/> 
+        <TransactionTable transactions={transactions} />
       </Suspense>
     </div>
   );
-};
-
-export default AccountsPage;
+}
